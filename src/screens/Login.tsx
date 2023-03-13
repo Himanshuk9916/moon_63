64fa991a1dfb028849387321a887c3d6f392ba88
Himanshukj1213 <himanshu.jaiswal@neosoftmail.com>
2023-03-13 12:19:06 +0530

    Login screen update

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
   SafeAreaView,
-  KeyboardAvoidingView,
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 import CustomInput from '../components/CustomInput';
 
@@ -14,6 +14,48 @@ const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
 function Login() {
+  const newUserSignUpView = () => {
+    return (
+      <View
+        style={{
+          backgroundColor: 'black',
+          paddingHorizontal: 10,
+          borderRadius: 10,
+          height: 100,
+          justifyContent: 'center',
+        }}>
+        <Text style={{color: 'white'}}>New User?</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <View style={{width: 220}}>
+            <Text style={{color: 'white'}}>
+              Signup with your mobile and start 15 days(s) free trial now
+            </Text>
+          </View>
+          <TouchableOpacity
+            style={{
+              justifyContent: 'center',
+              backgroundColor: 'white',
+              borderRadius: 10,
+            }}>
+            <Text style={{paddingHorizontal: 40}}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  const availableExchanges = [
+    'NSE',
+    'BSE',
+    'HCDEX',
+    'HCX',
+    'HCX-SX',
+    'HCX-EO',
+    'DSE',
+    'NHCE',
+    'NSEL',
+  ];
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerView}>
@@ -21,25 +63,52 @@ function Login() {
         <Text style={styles.loginTextHeader}>Login</Text>
       </View>
 
-      <KeyboardAvoidingView behavior="height" style={styles.inputView}>
+      <View style={styles.inputView}>
         <CustomInput placeholder="Username" />
         <CustomInput
           rightIcon={require('../Assets/password.png')}
           placeholder="Password"
           secureTextEntry={true}
         />
-      </KeyboardAvoidingView>
-
-      <View style={styles.signUpView}>
-        <TouchableOpacity style={styles.loginButton}>
-          <Text style={styles.loginText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={null}>
-          <Text>Forgot Password ?</Text>
-        </TouchableOpacity>
       </View>
 
-      <Text>Hello</Text>
+      <View>
+        <View style={styles.signUpView}>
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={null}>
+            <Text style={styles.forgotPText}>Forgot Password ?</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row', marginVertical: 15}}>
+          <Text>Haven't registered yet?Click here to </Text>
+          <TouchableOpacity>
+            <Text style={styles.registerText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+        <View>{newUserSignUpView()}</View>
+      </View>
+
+      <View>
+        <Text>Availbale Exchanges</Text>
+        {/* {availableExchanges.map((item)=>(
+            <View style={{display:'flex',flexDirection:'row'}}>
+            <Text>{item}</Text>
+            </View>
+        ))} */}
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <TouchableOpacity>
+            <Text style={styles.forgotPText}>Exchange Timings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.forgotPText}>About US</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.forgotPText}>Membership Details</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -49,9 +118,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-between',
     paddingHorizontal: 10,
+    paddingVertical: 20,
   },
   headerView: {
-    borderWidth: 1,
     height: '12%',
     justifyContent: 'space-around',
   },
@@ -76,6 +145,14 @@ const styles = StyleSheet.create({
   loginText: {
     paddingVertical: 10,
     alignSelf: 'center',
+  },
+  forgotPText: {
+    color: 'blue',
+    fontWeight: 'bold',
+  },
+  registerText: {
+    color: 'blue',
+    fontWeight: 'bold',
   },
 });
 
